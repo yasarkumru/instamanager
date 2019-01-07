438d93ca8814d669ff9f09f936eebd4b16e06d06
yasar kumru <yasar.kumru@mebitech.com>
2019-01-07 15:36:19 +0300

    refactoring

---
+++ insta-manager/src/main/java/com/polo/instamanager/instagram/InstagramService.java
@@ -0,0 +1,20 @@
+package com.polo.instamanager.instagram;
+
+import org.brunocvcunha.instagram4j.Instagram4j;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+/**
+ *
+ * @author yasar
+ */
+@Service
+@RequiredArgsConstructor
+public class InstagramService {
+    
+    private final Instagram4j instagram4j;
+    
+    
+
+}
--- insta-manager/src/main/java/config/Initializers.java
+++ insta-manager/src/main/java/config/Initializers.java
@@ -1,5 +1,6 @@
-package com.polo.instamanager;
+package config;
 
+import org.brunocvcunha.instagram4j.Instagram4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,10 +22,11 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class Initializers {
     private final TelegramBot telegramBot;
+    private final Instagram4j instagram4j;
     
     @Bean
     @Order(1)
-    public CommandLineRunner commandLineRunner() {
+    public CommandLineRunner initTelegramBot() {
         return args -> {
             TelegramBotsApi botsApi = new TelegramBotsApi();
             try {
@@ -34,5 +36,20 @@ public class Initializers {
                 log.error("Problem while initializing telegram bot",e);
             }
         };
+    }
+    
+    @Bean
+    @Order(2)
+    public CommandLineRunner initInstagram4J() {
+        return args -> {
+            instagram4j.setup();
+            instagram4j.login();
+        };
+    }
+    
+    
+    @Bean
+    public Instagram4j instagram4j() {
+        return Instagram4j.builder().username("yasar.evliyaoglu").password("12788yasar").build();
     }
 }
